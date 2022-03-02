2a0514983eab483d5e85269a15f2e7b03a38a116
NIMISHA MOHAN <85029169+nimmhn001@users.noreply.github.com>
2022-03-02 18:49:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,4 +96,38 @@ function mealRecipeModal(meal){
     mealContent.innerHTML = html;
     mealContent.parentElement.classList.add('showRecipe');
 }
+const button = document.getElementById("map");
+button.addEventListener("click", ()=>{
+    if(navigator.geolocation){
+        button.innerText = "Allow to detect location";
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        button.innerText = "Your browser not support";
+    }
+});
 
+//
+function onSuccess(position){
+    button.innerText = "Detecting your location...";
+    let {latitude, longitude} = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=c03e80486a604d41b931a3a3c01118a2`)
+    .then(response => response.json()).then(response =>{
+        console.log(response.json);
+        let allDetails = response.results[0].components;
+        console.table(allDetails);
+        let {county, postcode, country} = allDetails;
+        button.innerText = `${county} ${postcode}, ${country}`;
+    }).catch(()=>{
+        button.innerText = "Something went wrong";
+    });
+}
+function onError(error){
+    if(error.code == 1){
+        button.innerText = "You denied the request";
+    }else if(error.code == 2){
+        button.innerText = "Location is unavailable";
+    }else{
+        button.innerText = "Something went wrong";
+    }
+    button.setAttribute("disabled", "true");
+}
